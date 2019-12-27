f59e85df25287827cd59e0fb9bfd028dcb50f6ab
Preston Howell <pshowell23@gmail.com>
2019-12-27 10:44:06 -0500

    Set up solution

---
+++ largest-prime-factor/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	factor := getLargestPrimeFactor(13195)
+	fmt.Println("Largest Prime Factor: ", factor)
+}
+
+func getLargestPrimeFactor(num int) int {
+	// Find largest prime factor
+}
+
+func isPrime(num int) bool {
+	// Check if a number is prime
+}
