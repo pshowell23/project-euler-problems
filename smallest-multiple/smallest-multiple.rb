76da94dd29485eac50941a57c5105d19a4c7bd11
Preston Howell <pshowell23@gmail.com>
2019-12-28 18:53:09 -0500

    Remove unnecessary method

--- smallest-multiple/smallest-multiple.rb
+++ smallest-multiple/smallest-multiple.rb
@@ -20,17 +20,6 @@ def lcm(highestFactor)
     least_common_multiple
 end
 
-def get_prime_factors(num)
-    p_factors = []
-
-    Prime.each(num) do |p_num|
-        p_factors << p_num if num % p_num == 0
-        p_factors << num / p_num if Prime.prime?(num / p_num)
-    end
-
-    p_factors
-end
-
 # prime_factorization found at https://codereview.stackexchange.com/questions/57256/is-this-a-good-ruby-implementation-of-prime-factorization
 def prime_factorization(n)
     Prime.prime_division(n).flat_map {|factor, power| [factor] * power}
