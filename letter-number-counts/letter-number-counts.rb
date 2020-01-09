611e1537ed5e39d720594187b771ad4a2b753c83
Preston Howell <pshowell23@gmail.com>
2020-01-09 09:42:43 -0500

    Refactor and actually solve the problem

--- letter-number-counts/letter-number-counts.rb
+++ letter-number-counts/letter-number-counts.rb
@@ -2,7 +2,11 @@ require 'humanize'
 
 def letter_number_counts(x)
     sum = 0
-    numbers_to_words(x).each {|num| sum += num.length unless num == ' ' || num == '-'}
+    numbers_to_words(x).each do |num|
+        num.split('').each do |char|
+            sum += 1 unless char == ' ' || char == '-'
+        end
+    end
     "The sum of all letters used in the numbers 1 - #{x} is #{sum}"
 end
 
@@ -16,4 +20,4 @@ def numbers_to_words(x)
     numbers
 end
 
-puts letter_number_counts(1000) #Answer: 24,527
+print letter_number_counts(1000) #Answer: 21,124
