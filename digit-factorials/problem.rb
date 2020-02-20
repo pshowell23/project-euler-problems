37591fd75e0afb59ec7c6628cf408a7229dcc5d7
Preston Howell <pshowell23@gmail.com>
2020-02-20 09:12:50 -0500

    Readable ruby

--- digit-factorials/problem.rb
+++ digit-factorials/problem.rb
@@ -2,13 +2,13 @@ def digit_factorial(max_num)
     numbers = []
     for num in 3..max_num
         sum = 0
-        p "Checking: #{num}"
+        puts "Checking: #{num}"
         num.digits.each {|d| sum += factorial(d)}
         if sum == num
             numbers << num
         end
     end
-    p numbers
+    puts numbers
 end
 
 def factorial(num)
