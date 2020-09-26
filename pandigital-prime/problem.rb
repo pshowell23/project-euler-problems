18531889d5d3dad9a5e803fcb53cfbc083ddb77d
Preston Howell <pshowell23@gmail.com>
2020-09-25 22:20:44 -0400

    Make it cleaner

--- pandigital-prime/problem.rb
+++ pandigital-prime/problem.rb
@@ -1,17 +1,17 @@
 require 'prime'
 
 def highest_pandigital_prime
-  max_digit = 9
-  prime_pandigitals = get_prime_pandigitals_of_n_digits(max_digit)
-  while prime_pandigitals == []
-    max_digit -= 1
+    max_digit = 9
     prime_pandigitals = get_prime_pandigitals_of_n_digits(max_digit)
-  end
-  prime_pandigitals.max
+    while prime_pandigitals == []
+        max_digit -= 1
+        prime_pandigitals = get_prime_pandigitals_of_n_digits(max_digit)
+    end
+    prime_pandigitals.max
 end
 
 def get_prime_pandigitals_of_n_digits(max_digit)
-  (1..max_digit).to_a.permutation.map(&:join).map(&:to_i).select { |num| Prime.prime?(num) }
+    (1..max_digit).to_a.permutation.map(&:join).map(&:to_i).select { |num| Prime.prime?(num) }
 end
 
 puts highest_pandigital_prime() # Answer: 7652413
